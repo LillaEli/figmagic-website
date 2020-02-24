0d41578eb4253c0f10a9e7e642b3c0dcb531586a
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-02-24 23:51:48 +0100

    Update

---
+++ code.sh
@@ -0,0 +1,14 @@
+# Install and run globally
+npm install -g figmagic
+figmagic
+
+#... or with Yarn
+yarn global add figmagic
+figmagic
+
+# ...or locally
+npm install --save-dev figmagic
+./node_modules/figmagic/index.mjs
+
+# New for version 2.0: CLI parameters!
+figmagic --spacingUnit em --outputTokenFormat js
